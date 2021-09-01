a8c62d1b07d8e818ad77dc7be50ff6a5918e3c23
qaevelyn <89616707+qaevelyn@users.noreply.github.com>
2021-09-01 02:43:50 -0500

    Create test_title.py

---
+++ test_title.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+expected_title = 'Amazon.com. Spend less. Smile more.'
+base_url = 'https://www.amazon.com'
+search_title = 'Amazon.com: nike air max'
+
+
+@pytest.mark.alina
+def test_title(browser):
+    # navigate to Amazon.com home page
+    browser.get(base_url)
+    # verify that website title is Amazon.com
+    assert browser.title == expected_title
